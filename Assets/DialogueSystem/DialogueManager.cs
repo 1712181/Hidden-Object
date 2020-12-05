17c30ca59de407e06d63a6e597949bc4fe9ddf7f
tienhd <hodtienbrian@gmail.com>
2020-12-05 23:41:48 +0700

    initial

---
+++ Assets/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI speakerName, dialogue, navButtonText;
+    public Image speakerSprite;
+
+    private int currentIndex;
+    private Conversation currentConvo;
+    private static DialogueManager instance;
+    private Animator anim;
+    public static GameObject button;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;  
+            anim = GetComponent<Animator>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static void StartConversation(Conversation convo)
+    {
+        button = GameObject.Find("StartButton");
+        button.SetActive(false);
+        instance.anim.SetBool("isOpen", true);
+        instance.currentIndex = 0;
+        instance.currentConvo = convo;
+        instance.speakerName.text = "";
+        instance.dialogue.text = "";
+        instance.navButtonText.text = ">";
+
+        instance.ReadNext();
+    }
+
+    public void ReadNext()
+    {
+        if (currentIndex > currentConvo.GetLength())
+        {
+            instance.anim.SetBool("isOpen", false);
+            return;
+        }
+
+        speakerName.text = currentConvo.GetLineByIndex(currentIndex).speaker.GetName();
+        dialogue.text = currentConvo.GetLineByIndex(currentIndex).dialogue;
+        speakerSprite.sprite = currentConvo.GetLineByIndex(currentIndex).speaker.GetSprite();
+        currentIndex++;
+    }
+}
---
+++ Assets/MainMenu/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        LoadScene.Load(LoadScene.Scene.Dialogue);
+    }
+
+    // Update is called once per frame
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
